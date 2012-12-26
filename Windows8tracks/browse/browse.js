a0671eed8ef4a419c17109ba3d1f12749c79fa49
Noam Rubin <rubin.m.noam@gmail.com>
2012-12-26 01:05:55 -0800

    I can now playing music!

--- Windows8tracks/browse/browse.js
+++ Windows8tracks/browse/browse.js
@@ -4,11 +4,26 @@
     "use strict";
     var app = WinJS.Application;
     var nav = WinJS.Navigation;
+    var allMixes = {};
+
+    function addMixToAllMixes(mix) {
+        var id = mix.id;
+        allMixes[id] = mix;
+    }
+
+    function addMixesToAllMixes(mixes) {
+        for (var i = 0; i < mixes.length; i++) {
+            var mix = mixes[i];
+            addMixToAllMixes(mix);
+        }
+    }
+
     function getLatestMixes(eventargs) {
         console.log("getting latest mixes....");
         Networker.getLatestMixes("5", "1", renderLatestMixList);
     }
     function renderLatestMixList(mixes) {
+        addMixesToAllMixes(mixes);
         var listView = document.querySelector("#latestMixesListView").winControl;
         var dataList = new WinJS.Binding.List(mixes);
         listView.itemDataSource = dataList.dataSource;
@@ -19,11 +34,40 @@
         Networker.getFavoriteMixes(app.sessionState.userId,"5","1",renderFavoriteMixList);
     }
     function renderFavoriteMixList(mixes) {
+        addMixesToAllMixes(mixes);
         console.log("rendering favorite mixes");
         var listView = document.querySelector("#favoriteMixesListView").winControl;
         var dataList = new WinJS.Binding.List(mixes);
         listView.itemDataSource = dataList.dataSource;
     }
+
+    function playMix(eventargs) {
+        var mixId = eventargs.srcElement.parentNode.parentNode.querySelector(".mix-id").innerText;
+        app.sessionState.currentMix = allMixes[mixId];
+        nav.navigate("/listen/listen.html");
+    }
+
+    function addEventListenerstoMix(eventargs) {
+        var listViewDOM = eventargs.srcElement;
+        var listViewWinControl = listViewDOM.winControl;
+        if (listViewWinControl.loadingState === "complete") {
+            var mixes = listViewDOM.querySelectorAll(".mix");
+            for (var i = 0; i < mixes.length; i++) {
+                var mix = mixes[i];
+                mix.addEventListener("click", playMix);
+            }
+        }
+    }
+
+    function addLoadCompleteEventListenersToListViews( )
+    {
+        var listViews = document.querySelectorAll(".win-listview");
+        for (var i = 0; i < listViews.length; i++) {
+            var listView = listViews[i];
+            listView.addEventListener("loadingstatechanged", addEventListenerstoMix);
+        }
+    }
+
 
     WinJS.UI.Pages.define("/browse/browse.html", {
         // This function is called whenever a user navigates to this page. It
@@ -32,6 +76,8 @@
             // TODO: Initialize the page here.
             document.querySelector("#getLatestMixes").addEventListener("click", getLatestMixes);
             document.querySelector("#getFavoriteMixes").addEventListener("click", getFavoriteMixes);
+            addLoadCompleteEventListenersToListViews();
+            //document.querySelectorAll("[data-win-control='WinJS.UI.ListView']")
         },
 
         unload: function () {
--- Windows8tracks/listen/listen.js
+++ Windows8tracks/listen/listen.js
@@ -2,12 +2,32 @@
 // http://go.microsoft.com/fwlink/?LinkId=232511
 (function () {
     "use strict";
+    var app = WinJS.Application;
+    var nav = WinJS.Navigation;
+
+    function beginListening(eventargs) {
+        //eventargs.srcElement.removeEventListener("click");
+        var playToken = app.sessionState.playToken;
+        var mix = app.sessionState.currentMix;
+        var player = document.querySelector("audio");
+        Networker.playSong(playToken, mix.id, function (set) { player.src = set.track.track_file_stream_url; });
+    }
+
+    function setup() {
+        var playToken = app.sessionState.playToken;
+        var mix = app.sessionState.currentMix;
+        document.querySelector(".pagesubtitle").innerText = mix.name;
+        document.querySelector(".mix-pic").src = mix.cover_urls.sq100;
+        document.querySelector(".mix-description").innerText = mix.description;
+        document.querySelector(".mix-pic").addEventListener("click", beginListening);
+    }
 
     WinJS.UI.Pages.define("/listen/listen.html", {
         // This function is called whenever a user navigates to this page. It
         // populates the page elements with the app's data.
         ready: function (element, options) {
             // TODO: Initialize the page here.
+            setup();
         },
 
         unload: function () {
